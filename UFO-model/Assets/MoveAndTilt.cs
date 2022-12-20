b46a853f7f7670b831c1c3ab0fa979864211ce97
Parsley3000 <peterkeane2@gmail.com>
2022-12-20 00:13:20 +0000

    Up+down & camera
    Added up & down movement that comes to a smooth stop, also added camera tracking on UFO

--- UFO-model/Assets/MoveAndTilt.cs
+++ UFO-model/Assets/MoveAndTilt.cs
@@ -6,10 +6,17 @@ public class MoveAndTilt : MonoBehaviour
 {
     public float speed = 10.0f;
     public float tiltAmount = 15.0f;
-    public float tiltSmoothTime = 0.5f;
+    public float tiltSmoothTime = 5f;
+
+    // Add a new speed variable for the up and down movement
+    public float upDownSpeed = 2.0f;
+
+    // Add a new smooth time variable for the up and down movement
+    public float upDownSmoothTime = 0.3f;
 
     private float tiltVelocityX;
     private float tiltVelocityZ;
+    private Vector3 upDownVelocity = Vector3.zero;
 
     void Update()
     {
@@ -37,5 +44,24 @@ public class MoveAndTilt : MonoBehaviour
 
         // Smoothly rotate the object
         transform.rotation = Quaternion.Euler(Mathf.SmoothDampAngle(transform.rotation.eulerAngles.x, tiltX, ref tiltVelocityX, tiltSmoothTime), 0, Mathf.SmoothDampAngle(transform.rotation.eulerAngles.z, tiltZ, ref tiltVelocityZ, tiltSmoothTime));
+
+        // Check if the space bar is pressed
+        if (Input.GetKey(KeyCode.Space))
+        {
+            // Set the target position to the current position with an added offset in the y direction
+            Vector3 targetPosition = transform.position + Vector3.up;
+
+            // Smoothly move the object to the target position
+            transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref upDownVelocity, upDownSmoothTime);
+        }
+        // Check if the shift key is pressed
+        else if (Input.GetKey(KeyCode.LeftShift))
+        {
+            // Set the target position to the current position with a subtracted offset in the y direction
+            Vector3 targetPosition = transform.position + Vector3.down;
+
+            // Smoothly move the object to the target position
+            transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref upDownVelocity, upDownSmoothTime);
+        }
     }
 }
